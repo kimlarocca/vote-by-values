63605c4d32719e239963cf6a03698b9306b916e5
Kim LaRocca <kim@kimlarocca.com>
2025-11-22 16:22:12 -0500

    possible survey fix

--- pages/survey.vue
+++ pages/survey.vue
@@ -1,16 +1,24 @@
 <template>
-  <SurveyComponent :model="survey" />
+  <ClientOnly>
+    <SurveyComponent :model="survey" />
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
 import { Model } from "survey-core"
-import { SurveyComponent } from "survey-vue3-ui"
 import "survey-core/survey-core.min.css"
 import { json } from "~/assets/json.js"
 
 definePageMeta({
   layout: "blank",
 })
+
+// Dynamic import for survey-vue3-ui to avoid SSR issues
+const SurveyComponent = defineAsyncComponent(() =>
+  import("survey-vue3-ui").then(
+    (m) => m.SurveyComponent || m.default?.SurveyComponent || m.default
+  )
+)
 
 const survey = new Model(json)
 survey.onComplete.add((sender, options) => {
