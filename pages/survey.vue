138a1c1ef744c85a3835af021a116ed0d2aa3803
Kim LaRocca <kim@kimlarocca.com>
2025-11-24 11:32:12 -0500

    updated survey!

--- pages/survey.vue
+++ pages/survey.vue
@@ -7,7 +7,6 @@
 <script setup lang="ts">
 import { Model } from "survey-core"
 import "survey-core/survey-core.min.css"
-import { json } from "~/assets/json.js"
 
 definePageMeta({
   layout: "blank",
@@ -20,8 +19,68 @@ const SurveyComponent = defineAsyncComponent(() =>
   )
 )
 
-const survey = new Model(json)
-survey.onComplete.add((sender, options) => {
+const json = {
+  surveyId: "be7196bc-3e95-44d4-bdb9-1c983ac8c056",
+  surveyPostId: "700905ee-4e2f-4271-8ade-0da59dabc23d",
+}
+const surveyServiceUrl = "https://api.surveyjs.io/public/v1/Survey"
+const surveyIOMaxPostSize = 65536
+
+function loadSurvey(survey: Model, surveyId: string) {
+  survey.beginLoading()
+  fetch(surveyServiceUrl + "/getSurvey?surveyId=" + surveyId)
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      }
+      throw new Error("Could not load the survey JSON schema")
+    })
+    .then((data) => {
+      survey.fromJSON(data)
+      survey.endLoading()
+    })
+    .catch((error) => console.log(error))
+}
+
+function postResults(survey: Model, options: any, surveyPostId: string) {
+  const resultAsStr = JSON.stringify(survey.data)
+  // Display an error if survey results exceed the maximum post size
+  if (resultAsStr.length >= surveyIOMaxPostSize) {
+    options.showSaveError(survey.getLocalizationString("savingExceedSize"))
+    return
+  }
+  // Display the "Saving..." message (pass a string value to display a custom message)
+  options.showSaveInProgress()
+  const dataObj = { postId: surveyPostId, surveyResult: resultAsStr }
+  const dataStr = JSON.stringify(dataObj)
+  const headers = new Headers({ "Content-Type": "application/json; charset=utf-8" })
+  fetch(surveyServiceUrl + "/post/", {
+    method: "POST",
+    body: dataStr,
+    headers: headers,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Could not post the survey results")
+      }
+      // Display the "Success" message (pass a string value to display a custom message)
+      options.showSaveSuccess()
+      // Alternatively, you can clear all messages:
+      // options.clearSaveMessages();
+    })
+    .catch((error) => {
+      // Display the "Error" message (pass a string value to display a custom message)
+      options.showSaveError()
+      console.log(error)
+    })
+}
+
+const survey = new Model()
+survey.onComplete.add((sender: any, options: any) => {
   console.log(JSON.stringify(sender.data, null, 3))
+})
+loadSurvey(survey, json.surveyId)
+survey.onComplete.add((survey: any, options: any) => {
+  postResults(survey, options, json.surveyPostId)
 })
 </script>
