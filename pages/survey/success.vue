3f88d9db0042dbe1d5640a7ac1ebe03f77e3af31
Kim LaRocca <kim@kimlarocca.com>
2026-02-12 13:48:25 -0500

    small tweak

--- pages/survey/success.vue
+++ pages/survey/success.vue
@@ -1,7 +1,7 @@
 <script setup>
 definePageMeta({
   ssr: false,
-  layout: 'blank',
+  layout: "blank",
 })
 // No router needed - using native navigation
 </script>
@@ -17,22 +17,18 @@ definePageMeta({
     <section class="text-center bg-white rounded-xl p-8 max-w-2xl m-auto">
       <i class="pi pi-check-circle text-6xl text-green-500 mb-6" />
       <h1 class="mb-6">Thank You!</h1>
-      
-      <p class="text-lg mb-4">
-        Your survey responses have been successfully submitted.
-      </p>
-      
-      <p class="text-gray-600 mb-8">
-        Your positions on these important issues will help voters make informed decisions. 
+
+      <p class="mb-4">Your survey responses have been successfully submitted.</p>
+
+      <p class="mb-8">
+        Your positions on these important issues will help voters make informed decisions.
         We appreciate you taking the time to share your views.
       </p>
 
       <div class="flex flex-col sm:flex-row gap-4 justify-center">
-        <Button
-          label="Return to Home"
-          icon="pi pi-home"
-          @click="window.location.href = '/'"
-        />
+        <NuxtLink to="/" class="plain">
+          <Button label="Return to Home" icon="pi pi-home" />
+        </NuxtLink>
       </div>
     </section>
   </div>
