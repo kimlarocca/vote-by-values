7c1da64910aaa60419f4df00b9edc326759bbf86
Kim LaRocca <kim@kimlarocca.com>
2026-03-04 13:26:20 -0500

    fix: update tooltip text to 'Candidate Transparency Superstar' for consistency

--- components/Candidates.vue
+++ components/Candidates.vue
@@ -112,7 +112,7 @@ const getBlueskyUrl = (candidate) => {
                 Object.keys(candidate.survey_response).length > 49
               "
               class="absolute top-2 right-2 z-10"
-              v-tooltip="'This candidate has completed at least part our survey!'"
+              v-tooltip="'Candidate Transparency Superstar'"
             >
               <i class="pi pi-star-fill text-3xl text-yellow-400 drop-shadow-lg"></i>
             </div>
